8804c369043fdc209a1fcc6a7b6f067fb2cbb650
kanizfatima2 <kaniz686928@gmail.com>
2022-11-30 16:53:16 +0600

    s

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -23,13 +23,70 @@ if (uri) {
     console.log('Database Connected')
 }
 
+//verify JWT Token
+function verifyTokenJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 async function run() {
     try {
         const CategoriesCollection = client.db('phone-garage').collection('categories')
+        const AllPhonesData = client.db('phone-garage').collection('phones-data')
+        const usersCollection = client.db('phone-garage').collection('users');
 
+        //All Categories Phones data read
         app.get('/categories', async (req, res) => {
             const result = await CategoriesCollection.find({}).toArray();
             res.send(result)
+        })
+
+        //Read by category phones data
+        app.get('/categories/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { phone_id: id }
+            const cursor = AllPhonesData.find(query);
+            // console.log(query)
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        //Create user and save to database 
+        app.post('/users', async (req, res) => {
+            const result = await usersCollection.insertOne(req.body)
+            res.send(result)
+        })
+
+        //Get user and match email address and generate token
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+        //Get all Users in Dashboard
+        app.get('/users', async (req, res) => {
+            const users = await usersCollection.find({}).toArray();
+            res.send(users)
         })
 
     }
